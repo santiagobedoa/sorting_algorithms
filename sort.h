4fef8ec2c90c5c81b6f4d67a246a0c4fc5fd4cf9
Santiago Bedoya <santiagobedoa@gmail.com>
2022-06-20 18:55:58 -0500

    Mon Jun 20 18:55:58 -05 2022

--- sort.h
+++ sort.h
@@ -31,5 +31,6 @@ void quick_sort(int *array, size_t size);
 void swap(int *array, int first, int second, size_t size);
 int partition(int *array, int start, int end, size_t size);
 void recursion_sort(int *array, int start, int end, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
